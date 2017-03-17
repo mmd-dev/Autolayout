e8d11a5ade0b50dea312c03027313bc95f1e9dbc
shadowjilu <shadowjilu@gmail.com>
2017-03-17 21:28:27 +0800

    auto layout question again

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         updateUI()
     }
-    
+    var loggedInUser: User? { didSet{ updateUI() } }
     var secure = false { didSet { updateUI() } }
     //这里的secure除了初始值是非真，还表示每次调用secure时都运行updateUI？
     
@@ -26,6 +26,10 @@ class ViewController: UIViewController {
         passwordField.isSecureTextEntry = secure
         passwordLabel.text = secure ? "Secured Password" : "Password"
         //?:的用法是看高级运算符文档吗？
+    }
+    
+    @IBAction func login() {
+        loggedInUser = User.login(login: loginField.text ?? "", password: passwordField.text ?? "")
     }
     
     @IBAction func toggleSecurity() {
