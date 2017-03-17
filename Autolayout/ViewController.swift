1787a0b9cfe500aadbfde9b7166309e10f37d4b2
shadowjilu <shadowjilu@gmail.com>
2017-03-17 19:59:06 +0800

    no message

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -10,7 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-   
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    var secure = false { didSet { updateUI() } }
+    //这里的secure除了初始值是非真，还表示每次调用secure时都运行updateUI？
+    
+    private func updateUI() {
+        passwordField.isSecureTextEntry = secure
+        passwordLabel.text = secure ? "Secured Password" : "Password"
+        //?:的用法是看高级运算符文档吗？
+    }
+    
+    @IBAction func toggleSecurity() {
+        secure = !secure
+    }
 }
 
