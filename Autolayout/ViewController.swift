9c472a033894cf4285a6baec0e7826b334d0514f
shadowjilu <shadowjilu@gmail.com>
2017-03-19 18:55:39 +0800

    no message

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var passwordLabel: UILabel!
-
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
@@ -26,6 +29,9 @@ class ViewController: UIViewController {
         passwordField.isSecureTextEntry = secure
         passwordLabel.text = secure ? "Secured Password" : "Password"
         //?:的用法是看高级运算符文档吗？
+        nameLabel.text = loggedInUser?.name
+        companyLabel.text = loggedInUser?.company
+        image = (loggedInUser?.image)!
     }
     
     @IBAction func login() {
@@ -35,5 +41,57 @@ class ViewController: UIViewController {
     @IBAction func toggleSecurity() {
         secure = !secure
     }
+    var image: UIImage {
+        get {
+            return imageView.image!
+        }
+        set {
+            imageView.image = newValue
+            if let constrainedView = imageView {
+                if let newImage = newValue {
+                    aspectRatioConstraint = NSLayoutConstraint(
+                        item: constrainedView,
+                        attribute: .width,
+                        relatedBy: .equal,
+                        toItem: constrainedView,
+                        attribute: .height,
+                        multiplier: newImage.aspectRatio,
+                        constant: 0)
+                } else {
+                    aspectRatioConstraint = nil
+                }
+            }
+        }
+    }
+    
+    var aspectRatioConstraint: NSLayoutConstraint? {
+        willSet {
+            if let existingConstraint = aspectRatioConstraint {
+                view.removeConstraint(existingConstraint)
+            }
+        }
+        didSet {
+            if let newConstraint = aspectRatioConstraint {
+                view.addConstraint(newConstraint)
+            }
+        }
+    }
+    
 }
 
+extension User {
+    var image: UIImage? {
+        if let image = UIImage(named: login) {
+            return image
+        } else {
+            return UIImage(named: "unknown_user")
+        }
+    }
+}
+
+extension UIImage {
+    var aspectRatio: CGFloat {
+        return size.height != 0 ? size.width / size.height : 0
+    }
+    
+}
